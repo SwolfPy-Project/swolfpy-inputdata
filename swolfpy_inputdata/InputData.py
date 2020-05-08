df774e7db82cf31a122bf24521b4f1dfbe38b673
Mojtaba Sardar <msardar2@ncsu.edu>
2020-05-08 17:12:45 -0400

    Document

--- swolfpy_inputdata/InputData.py
+++ swolfpy_inputdata/InputData.py
@@ -11,7 +11,20 @@ from pathlib import Path
 import ast
 
 class InputData(MC):
+    """
+    ``InputData`` class reads the input data from the csv file and load them as class attributes. This class is inherited from the ``MC`` class. 
+    
+    Main functionalities include:
+    loading data, updating data and generating random number for data based on the defined probabiliy distributions.
+    """
     def __init__(self,input_data_path,eval_parameter=False):
+        """Initialize ``InputData`` class
+        
+        :param input_data_path: absolute path to the input data file
+        :type input_data_path: str
+        :param eval_parameter: If the parameters are tuple instead of str, it will evalute their real value.
+        :type eval_parameter: bool, optional
+        """
         self.input_data_path = input_data_path            
         self.Data=pd.read_csv(self.input_data_path,dtype={'amount':float,'uncertainty_type':float,'loc':float,
                                                       'scale':float,'shape':float,'minimum':float,'maximum':float})
@@ -22,23 +35,36 @@ class InputData(MC):
         # Setting uncertainty type to 0 : Undefined ; when it is not defined
         self.Data['uncertainty_type'].fillna(0,inplace=True)
         #self.Data=self.Data.where((pd.notnull(self.Data)),None) 
-        self.Input_list = {}
+        self.Input_dict = {}
         self.keys = self.Data.columns[3:]
         for i in range(len(self.Data)):
-            if self.Data.Category[i] not in self.Input_list.keys():
+            if self.Data.Category[i] not in self.Input_dict.keys():
                 exec("self.%s = {}" % self.Data.Dictonary_Name[i])
-                exec("self.Input_list[self.Data.Category[i]] = self.%s" % self.Data.Dictonary_Name[i])
+                exec("self.Input_dict[self.Data.Category[i]] = self.%s" % self.Data.Dictonary_Name[i])
                 exec("self.%s[self.Data.Parameter[i]] = dict(zip(self.keys,self.Data.loc[i,'Name':]))" % self.Data.Dictonary_Name[i])
             else:
                 exec("self.%s[self.Data.Parameter[i]] = dict(zip(self.keys,self.Data.loc[i,'Name':]))" % self.Data.Dictonary_Name[i])
 
 ### Update_Input
     def Update_input(self,NewData):
+        """ Get a new DataFrame and update the ``data`` in ``InputData`` class.
+        
+        :param NewData:
+        :type NewData: 'pandas.DataFrame'
+        """
         for i in NewData.index:
             exec("self.%s[NewData.Parameter[i]] = dict(zip(self.keys,NewData.loc[i,'Name':]))" %NewData.Dictonary_Name[i])
             self.Data.loc[i]=NewData.loc[i]
             
 ### Monte_carlo          
     def setup_MC(self,seed=None):
-        super().__init__(self.Input_list)
+        """ Initialize the parent class (``MC``) and create ``MCRandomNumberGenerator`` based on the data for uncertainty distributions via
+        calling ``MC.setupMC()`` method.
+        
+        :param seed: seed for random number generation
+        :type seed: int, optional
+        
+        .. seealso:: Class_MC_
+        """
+        super().__init__(self.Input_dict)
         super().setup_MC(seed)
--- swolfpy_inputdata/MC.py
+++ swolfpy_inputdata/MC.py
@@ -10,16 +10,67 @@ import pandas as pd
 from pathlib import Path
 
 class MC():
-    def __init__(self,input_list):
-        self.input_list = input_list
+    """
+    This class generates random number for Monte-Carlo simulations. This class is the interface to `stats_arrays <https://pypi.org/project/stats-arrays/>`_ package.
+    
+    The example below is showing the usage of ``stats_arrays``.
+    
+    :Example:
+        
+    >>> from stats_arrays import *
+    >>> my_variables = UncertaintyBase.from_dicts(
+    ...     {'loc': 2, 'scale': 0.5, 'uncertainty_type': NormalUncertainty.id},
+    ...     {'loc': 1.5, 'minimum': 0, 'maximum': 10, 'uncertainty_type': TriangularUncertainty.id}
+    ... )
+    >>> my_variables
+    array([(2.0, 0.5, nan, nan, nan, False, 3),
+           (1.5, nan, nan, 0.0, 10.0, False, 5)],
+        dtype=[('loc', '<f8'), ('scale', '<f8'), ('shape', '<f8'),
+               ('minimum', '<f8'), ('maximum', '<f8'), ('negative', '?'),
+               ('uncertainty_type', 'u1')])
+    >>> my_rng = MCRandomNumberGenerator(my_variables)
+    >>> my_rng.next()
+    array([ 2.74414022,  3.54748507])
+    
+    .. _Class_MC: 
+    
+    
+    """
+    def __init__(self,input_dict):
+        """ Initialize ``MC`` class
+        
+        :param input_dict: list of dictionaries that include input data (see the example)
+        :type input_dict: list
+        
+        >>> from swolfpy_inputdata import MC
+        >>> input_dict={'Cat1': {'Par1': {'Name': 'Name1','amount': 1.0,'unit': 'Unit1',
+        ...                                 'uncertainty_type': 3,'loc': 1,'scale':0.2 ,'shape': None,
+        ...                                 'minimum': None,'maximum': None,
+        ...                                 'Reference': None,'Comment': None},
+        ...                     'Par2': {'Name': 'Name2','amount': 1.5,'unit': 'Unit2',
+        ...                                 'uncertainty_type': 3,'loc': 1.5,'scale': 0.4,'shape': None,
+        ...                                 'minimum': None,'maximum': None,
+        ...                                 'Reference': None,'Comment': None}}}
+        >>> test_MC = MC(input_dict)
+        >>> test_MC.setup_MC()
+        >>> test_MC.gen_MC()
+        [(('Cat1', 'Par1'), 1.0554408376879747),
+         (('Cat1', 'Par2'), 1.9366617123732333)]
+        """
+        self.input_dict = input_dict
         
     def setup_MC(self,seed=None):
+        """ Creates `MCRandomNumberGenerator` and store it in ``MC.rand`` attribute
+    
+        :param seed: seed for random number generation
+        :type seed: int, optional
+        """
         self.list_var = list()
         self.sens_analysis={}
         self.run_index=0
         self.max_run = 10000000
         k=0
-        for x in self.input_list.values():
+        for x in self.input_dict.values():
             for y in x:
                 self.list_var.append(x[y])
                 if 'list' in x[y].keys():
@@ -30,6 +81,11 @@ class MC():
         self.rand = MCRandomNumberGenerator(self.Vars,seed=seed)
       
     def gen_MC(self):
+        """Generate random numbers and update data. It also returns a list of tuples include the name of parameter and generted number.
+        
+        :return: List of tuples include the name of parameter and generted number: ``[((parameter_category,parameter),generated_number)]``
+        :rtype: list
+        """
         data = self.rand.next()
         if len(self.sens_analysis) > 0:
             for x in self.sens_analysis.keys():
@@ -39,15 +95,17 @@ class MC():
                 raise ValueError('Number of runs are more than the number of defined inputs') 
         i=0
         variables = []
-        for x in self.input_list.keys():
-            for y in self.input_list[x]:
+        for x in self.input_dict.keys():
+            for y in self.input_dict[x]:
                 if not np.isnan(data[i]):  
-                    self.input_list[x][y]['amount'] = data[i]
+                    self.input_dict[x][y]['amount'] = data[i]
                     variables.append( ( (x , y) , data[i]) )
                 i+=1
         return(variables)
 		
     def create_uncertainty_from_inputs(self,sheet_name,process_data,seed=None):
+        """Creates `MCRandomNumberGenerator` based on the data related to materials. 
+        """
         self.process_data=process_data
         self.process_data_1=pd.read_excel(Path(__file__).parent/'Data/Material properties - process modles.xlsx', sheet_name = sheet_name, index_col = 'Parameter')
         self.uncertain_dict = dict()
@@ -73,6 +131,8 @@ class MC():
             self.rng[key] = MCRandomNumberGenerator(self.variables[key],seed=seed)
 		
     def uncertainty_input_next(self):
+        """Generate random numbers and update the `process_data`
+        """
         data = dict()
         variables = list()
         for key in self.rng.keys():
