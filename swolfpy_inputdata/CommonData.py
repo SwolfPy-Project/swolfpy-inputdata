47a03d935659fa79f49a14a5476c2b2c328b9bd9
Mojtaba Sardarmehni <msardar2@ncsu.edu>
2021-03-15 18:03:34 -0400

    Rename: third_Class_Mail -> Third_Class_Mail

--- swolfpy_inputdata/CommonData.py
+++ swolfpy_inputdata/CommonData.py
@@ -22,16 +22,13 @@ class CommonData(InputData):
 ### Materials
         self.Index = ['Yard_Trimmings_Leaves', 'Yard_Trimmings_Grass', 'Yard_Trimmings_Branches', 'Food_Waste_Vegetable',
                       'Food_Waste_Non_Vegetable', 'Wood', 'Wood_Other', 'Textiles', 'Rubber_Leather', 'Newsprint',
-                      'Corr_Cardboard', 'Office_Paper', 'Magazines', 'third_Class_Mail', 'Folding_Containers', 'Paper_Bags',
+                      'Corr_Cardboard', 'Office_Paper', 'Magazines', 'Third_Class_Mail', 'Folding_Containers', 'Paper_Bags',
                       'Mixed_Paper', 'Paper_Non_recyclable', 'HDPE_Translucent_Containers', 'HDPE_Pigmented_Containers',
                       'PET_Containers', 'Plastic_Other_1_Polypropylene', 'Plastic_Other_2', 'Mixed_Plastic', 'Plastic_Film',
                       'Plastic_Non_Recyclable', 'Ferrous_Cans', 'Ferrous_Metal_Other', 'Aluminum_Cans', 'Aluminum_Foil',
                       'Aluminum_Other', 'Ferrous_Non_recyclable', 'Al_Non_recyclable', 'Glass_Brown', 'Glass_Green',
                       'Glass_Clear', 'Mixed_Glass', 'Glass_Non_recyclable', 'Misc_Organic', 'Misc_Inorganic', 'E_waste',
-                      'Aerobic_Residual', 'Anaerobic_Residual', 'Bottom_Ash', 'Fly_Ash', 'Diapers_and_sanitary_products',
-                      'Waste_Fraction_47', 'Waste_Fraction_48', 'Waste_Fraction_49', 'Waste_Fraction_50', 'Waste_Fraction_51',
-                      'Waste_Fraction_52', 'Waste_Fraction_53', 'Waste_Fraction_54', 'Waste_Fraction_55', 'Waste_Fraction_56',
-                      'Waste_Fraction_57', 'Waste_Fraction_58', 'Waste_Fraction_59', 'Waste_Fraction_60']
+                      'Aerobic_Residual', 'Anaerobic_Residual', 'Bottom_Ash', 'Fly_Ash', 'Diapers_and_sanitary_products']
 
         ### Read Material properties
         self.Material_Properties = pd.read_csv(Path(__file__).parent / "Data/Material properties.csv",
